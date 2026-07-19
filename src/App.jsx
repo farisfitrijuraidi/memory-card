83b128078160eebacae03266c927b02b1a1ac3fb
Faris Fitri <169568145+farisfitrijuraidi@users.noreply.github.com>
2026-07-19 14:45:32 +0800

    set up local character data and asset mapping

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,94 @@
 import './App.css'
 import { useState, useEffect } from 'react';
+import luffy from './Assets/luffy.png';
+import zoro from './Assets/zoro.avif';
+import nami from './Assets/nami.png';
+import sanji from './Assets/sanji.webp';
+import usopp from './Assets/usopp.webp';
+import chopper from './Assets/chopper.avif';
+import robin from './Assets/robin.avif';
+import franky from './Assets/franky.jpg';
+import brook from './Assets/brook.jpg';
+import jinbe from './Assets/jinbe.jpeg';
 
-export const TestLinter = () => {
-  const [count, setCount] = useState(0);
-  const [multiplier, setMultiplier] = useState(2);
+const characterMap = [
+  {
+    id: 1,
+    name: 'Monkey D. Luffy',
+    image: luffy
+  },
+  {
+    id: 2,
+    name: 'Roronoa Zoro',
+    image: zoro
+  },
+  {
+    id: 3,
+    name: 'Nami',
+    image: nami
+  },
+  {
+    id: 5,
+    name: 'Vinsmoke Sanji',
+    image: sanji
+  },
+  {
+    id: 6,
+    name: 'Tony Tony Chopper',
+    image: chopper
+  },
+  {
+    id: 4,
+    name: 'Usopp',
+    image: usopp
+  },
+  {
+    id: 7,
+    name: 'Robin',
+    image: robin
+  },
+  {
+    id: 8,
+    name: 'Franky',
+    image: franky
+  },
+  {
+    id: 9,
+    name: 'Brook',
+    image: brook
+  },
+  {
+    id: 10,
+    name: 'Jinbe',
+    image: jinbe
+  },
+];
 
-//   useEffect(() => {
-//     // We are using the multiplier variable inside here
-//     const result = count * multiplier;
-//     console.log(result);
-//   }, [count, multiplier]); // Look here: multiplier is completely missing!
+export const App = () => {
+  
+
+  // useEffect(() => {
+  //   const fetchOnePieceCharacter = async () => {
+  //     const url = `https://api.jikan.moe/v4/characters/40/pictures`;
+  //     try {
+  //       const response = await fetch(url);
+  //       if (!response.ok) {
+  //         throw new Error('The server rejected our request');
+  //       }
+  //       const data = await response.json();
+  //       console.log(data);
+  //     } catch (error) {
+  //       console.error('Failed to fetch One Piece Character :', error);
+  //     }
+  //   }
+  //   fetchOnePieceCharacter();
+  // }, [])
 
   return (
-    <button onClick={() => setCount(c => c + 1)}>
-      Count: {count}
-    </button>
+    <>
+      {characterMap.map(obj => {
+        return <img key={obj.id} src={obj.image}></img>
+      })}
+    </>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
-import { TestLinter } from './App.jsx';
+import { App } from './App.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <TestLinter />
+    <App />
   </StrictMode>,
 )
