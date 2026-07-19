65d0f97cf0e0b42bc4fcccd5f4a60b4af8d087bc
Faris Fitri <169568145+farisfitrijuraidi@users.noreply.github.com>
2026-07-19 10:28:59 +0800

    Test functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,19 @@
 import './App.css'
+import { useState, useEffect } from 'react';
+
+export const TestLinter = () => {
+  const [count, setCount] = useState(0);
+  const [multiplier, setMultiplier] = useState(2);
+
+//   useEffect(() => {
+//     // We are using the multiplier variable inside here
+//     const result = count * multiplier;
+//     console.log(result);
+//   }, [count, multiplier]); // Look here: multiplier is completely missing!
+
+  return (
+    <button onClick={() => setCount(c => c + 1)}>
+      Count: {count}
+    </button>
+  );
+}
